59ef7755fdbc04eb1f2f0cf2f6015fa58d90f632
Emilio Manuel Cebu <52725264+EMC1010@users.noreply.github.com>
2023-04-16 21:44:35 +0800

    GroceryApp-Activity3(Week7)

---
+++ app/src/main/java/com/cebu/groceryapp_acitivity3week7/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.cebu.groceryapp_acitivity3week7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
